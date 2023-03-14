1ff8062820ec9590af4d920cbb355b6908ad4d34
chedaah <integer94@gmail.com>
2023-03-14 11:06:10 +0900

    add prime counter

---
+++ src/function.py
@@ -0,0 +1,16 @@
+def is_prime(n):
+    if n == 1:
+        return False
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+
+    return True
+
+def count_prime_number(n):
+    count = 0
+    for i in range(1, n+1):
+        if is_prime(i):
+            count += 1
+
+    return count
